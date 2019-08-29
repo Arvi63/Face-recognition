0499f3693d89063b9c0b0664d91365ddbbe7707c
suraj <sbista63@gmail.com>
2019-08-29 22:06:45 +0545

    TIll now complete

--- test.py
+++ test.py
@@ -28,10 +28,10 @@ import pandas as pd
 
  
 # df=pd.read_csv("StudentDetails.csv")
-df=pd.read_csv("studentdups.csv")
+# df=pd.read_csv("studentdups.csv")
 # df = df.drop('Unnamed: 0', axis=1)
 
-a =4	
+# a =4	
 
 # for items in df['ID']:
 # 	if items == int(a) :
@@ -53,7 +53,16 @@ a =4
 # 	print("Id exist")
 
 
-if int(a) in df['ID'].values:
-	print("ID chosen")
-else:
-	print("not chosen")
+# if int(a) in df['ID'].values:
+# 	print("ID chosen")
+# else:
+# 	print("not chosen")
+
+name = "suraj"
+id = 1
+a = name+'_'+str(id)
+print(a)
+df=pd.read_csv("studentdups.csv")
+b = df[df['Name'] == a].index
+df = df.drop(b)
+print(df)
